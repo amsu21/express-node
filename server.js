fe5a477637d841ac9a5a8863a8728ca1743a45fa
Amsu Warner <aswb4@hotmail.com>
2021-07-14 23:43:02 -0400

    Added new GET route for random strings

--- server.js
+++ server.js
@@ -13,9 +13,13 @@ let mockUserData =[
     {name: 'Warner'},
 ]
 
+// NEW USERS MOCK JSON DATA
+let newUserData =[
+]
+
 // GET USERS 
 app.get('/users', function(req, res){
-    console.log("Got dem thangs!")
+    console.log("Got dem thangs! Users locked in!");
     res.json({
         success: true,
         message: 'got dem thangs!',
@@ -23,7 +27,15 @@ app.get('/users', function(req, res){
     })
 })
 
+// ADD NEW USERS
+app.get('/newusers', function(req, res){
+    console.log("New users locked in!");
+})
 
+
+
+
+// RANDOM STRING FUNCTION
 
 // APP LISTENING AND TALKING
 app.listen(8000, function(){
