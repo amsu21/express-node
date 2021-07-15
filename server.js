c69f96471e8be5015920a6e18b3d6c1a93af475e
Amsu Warner <aswb4@hotmail.com>
2021-07-14 21:40:43 -0400

    Added app port and GET route

--- server.js
+++ server.js
@@ -13,3 +13,19 @@ let mockUserData =[
     {name: 'Warner'},
 ]
 
+// GET USERS 
+app.get('/users', function(req, res){
+    console.log("Got dem thangs!")
+    res.json({
+        success: true,
+        message: 'got dem thangs!',
+        users: mockUserData
+    })
+})
+
+
+
+// APP LISTENING AND TALKING
+app.listen(8000, function(){
+    console.log("Dat thang on!");
+})
