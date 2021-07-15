9c07b1f69920865f6045f7ad328157ee4f8bdfb3
Amsu Warner <aswb4@hotmail.com>
2021-07-14 21:36:28 -0400

    Added mock JSON data

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const express = require('express');
 
 // EXPRESS CONSTRUCTOR
 const app = express();
+
+
+// RETURN MOCK JSON DATA
+let mockUserData =[
+    {name: 'Amsu'},
+    {name: 'Sulayman'},
+    {name: 'Baasir'},
+    {name: 'Warner'},
+]
+
